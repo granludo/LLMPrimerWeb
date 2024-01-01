541e6628395bcd125fe162742d257b5c69b84f87
Ludo (Marc Alier) <granludo@gmail.com>
2024-01-01 16:09:37 +0100

    init

---
+++ obsidian-to-hugo-home-ludo.py
@@ -0,0 +1,15 @@
+from obsidian_to_hugo import ObsidianToHugo
+
+def process_file(file_contents: str) -> str:
+    modified_contents = file_contents.replace("/imgs/", "https://wasabi.essi.upc.edu/llmprimer/IMGS/")
+    modified_contents = modified_contents.replace("/IMGS/", "https://wasabi.essi.upc.edu/llmprimer/IMGS/")
+    return modified_contents
+
+
+obsidian_to_hugo = ObsidianToHugo(
+    obsidian_vault_dir="/Users/ludo/Code/LLMPrimerWeb-obsidian",
+    hugo_content_dir="/Users/ludo/Code/LLMPrimerWeb-hugo/content",
+    processors=[process_file],
+)
+
+obsidian_to_hugo.run()
