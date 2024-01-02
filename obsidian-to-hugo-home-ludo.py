b0d1e4eeaa145508c4593ae0f4506a91623cd064
Ludo (Marc Alier) <granludo@gmail.com>
2024-01-02 14:23:08 +0100

    up

--- obsidian-to-hugo-home-ludo.py
+++ obsidian-to-hugo-home-ludo.py
@@ -1,8 +1,8 @@
 from obsidian_to_hugo import ObsidianToHugo
 
 def process_file(file_contents: str) -> str:
-    modified_contents = file_contents.replace("/imgs/", "https://wasabi.essi.upc.edu/llmprimer/IMGS/")
-    modified_contents = modified_contents.replace("/IMGS/", "https://wasabi.essi.upc.edu/llmprimer/IMGS/")
+    modified_contents = file_contents.replace("/imgs/", "https://llmprimer.com/IMGS/")
+    modified_contents = modified_contents.replace("/IMGS/", "https://llmprimer.com/IMGS/")
     return modified_contents
 
 
